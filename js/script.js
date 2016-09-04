016ddad00d688afc06fdb902232b1aa2b4a0da38
KiranKK7 <kiranriddle.kc29@gmail.com>
2016-09-04 00:31:20 -0700

    Views and Added Input for new Notes

--- js/script.js
+++ js/script.js
@@ -30,3 +30,43 @@ var blog2 = new Blog({
 // instantiate a Collection
 
 var blogs = new Blogs([blog1,blog2]);
+
+// Backbone View for one blog
+
+var BlogView = Backbone.View.extend({
+ 
+ model: new Blog(),
+ tagName: 'tr',
+ initialize: function () {
+   this.template = _.template($('.notes-list-template').html());
+ },
+
+ render: function () {
+ 	this.$el.html(this.template(this.model.toJSON()));
+ }
+
+});
+
+// Backbone View for all blog
+
+var BlogsView = Backbone.View.extend({
+
+model: blogs,
+el: $('.notes-list'),
+initialize: function() {
+	this.model.on('add', this.render(), this);
+},
+
+render: function () {
+	
+	var self = this;
+	this.el.html('');
+
+	_each(this.model.toArray(), function(blog) {
+     
+     self.el.append((new BlogsView({model: blog})).render().$el);
+	});
+}
+
+});
+
